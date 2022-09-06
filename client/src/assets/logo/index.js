8b26e74c9b816142ffb7fe64a550a0bd385b8a76
rolandexplore93 <roland2rule@gmail.com>
2022-09-06 06:08:21 +0100

    Client side set up

---
+++ client/src/assets/logo/index.js
@@ -0,0 +1,9 @@
+import tetrawriteLogoWithText from 'tetrawrite-logo-text.svg';
+import tetrawriteLogo from 'tetrawrite-logo-skyblue.svg';
+
+const logo = {
+    tetrawriteLogoWithText,
+    tetrawriteLogo
+}
+
+export default logo
