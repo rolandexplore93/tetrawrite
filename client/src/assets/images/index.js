63bddb2d89f5f4d22ace9716c9474df9d4183e2f
rolandexplore93 <roland2rule@gmail.com>
2022-10-03 12:11:01 +0100

    Modify homepage layout

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -1,7 +1,11 @@
 import homeBannerImage from './banner-image.svg';
+import homeAboutImage from './home-about-image.svg';
+import cloudImage from './cloud.svg';
 
 const images = {
-    homeBannerImage
+    homeBannerImage,
+    homeAboutImage,
+    cloudImage
 }
 
 export default images
